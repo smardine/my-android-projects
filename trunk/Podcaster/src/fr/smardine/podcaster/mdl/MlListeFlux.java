d6ec6e837c590463c9fa44a492bf76a9e3cc3e5c
s.mardine@gmail.com <s.mardine@gmail.com@02e0656c-9b8e-47bc-7b83-1cbf0f5ee4f9>
2012-12-18 13:22:06 +0000

    le clic sur la vue principale pour afficher la liste des episodes fonctionne mais si on revient en arriere et que l'on clique directement sur la tab , ca ne fonctionne plus, ca garde en memoire le flux que l'on a cliqué precedement

--- trunk/Podcaster/src/fr/smardine/podcaster/mdl/MlListeFlux.java
+++ trunk/Podcaster/src/fr/smardine/podcaster/mdl/MlListeFlux.java
@@ -9,4 +9,12 @@ public class MlListeFlux extends ArrayList<MlFlux> {
 	 */
 	private static final long serialVersionUID = -1331250459788965471L;
 
+	public MlListeEpisode GetAllEpisode() {
+		MlListeEpisode lstRetour = new MlListeEpisode();
+		for (MlFlux unFLux : this) {
+			lstRetour.addAll(unFLux.getListeEpisode());
+		}
+		return lstRetour;
+	}
+
 }
