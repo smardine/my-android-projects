664f779278f81acecb5201ac38ebb79a258d514c
s.mardine <s.mardine@gmail.com>
2013-01-04 15:35:23 +0000

    ajout d'une alerte dialog sur le bouton en bas a droite pour commencer a tester l'insertion en base a partir d'info recuperée d'un flux

--- Podcaster/src/fr/smardine/podcaster/listener/ButtonAjoutFluxClickListener.java
+++ Podcaster/src/fr/smardine/podcaster/listener/ButtonAjoutFluxClickListener.java
@@ -6,21 +6,23 @@ import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
 import fr.smardine.podcaster.R;
-import fr.smardine.podcaster.alertDialog.AlertDialogFactory;
 import fr.smardine.podcaster.alertDialog.clickListener.buttonClick.AlertDialogOuiNonClickListener;
 
 public class ButtonAjoutFluxClickListener implements OnClickListener {
 
 	private final Context context;
+	private final View view;
 
-	public ButtonAjoutFluxClickListener(Context baseContext) {
+	public ButtonAjoutFluxClickListener(Context baseContext, View p_view) {
 		this.context = baseContext;
+		this.view = p_view;
 	}
 
 	@Override
 	public void onClick(View p_view) {
-		AlertDialogFactory adFactory = new AlertDialogFactory(context);
+		// AlertDialogFactory adFactory = new AlertDialogFactory(context);
 		Builder ad = new AlertDialog.Builder(context);
+
 		ad.setTitle("Ajout de flux");
 		ad.setIcon(R.drawable.ad_attention);
 		ad.setMessage("http://www.rtl.fr/podcast/laurent-gerra.xml");
@@ -29,6 +31,7 @@ public class ButtonAjoutFluxClickListener implements OnClickListener {
 				"http://www.rtl.fr/podcast/laurent-gerra.xml", this.context);
 		ad.setPositiveButton("Ok", ouinonClickListener);
 		ad.setNegativeButton("Annuler", null);
+		ad.setView(this.view);
 		ad.show();
 
 	}
